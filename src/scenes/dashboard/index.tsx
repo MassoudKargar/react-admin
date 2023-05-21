b5c450fe28574c277bdacedfbe333b1342a255ac
m.kargar <massoudkargar@outlook.com>
2023-05-21 17:52:30 +0330

    feat: add a new title

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -5,7 +5,7 @@ export const Dashboard = () => {
     return (
         <Box m="20px">
             <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-                <Header title="DASHBOARD" subtitle="Welcome to yout dashboard" />
+                <Header title="DASHBOARD" subtitle="Hi welcome to Dashboard" />
             </Box>
         </Box>
     )
